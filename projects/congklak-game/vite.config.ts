9eaa75fd66a3244afedc1cfa03d0252f915acbbd
xirecentxz <129609019+xirecentxz@users.noreply.github.com>
2026-07-07 15:58:43 +0700

    Delete projects/congklak-game directory

--- projects/congklak-game/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// NOTE: ganti "base" di bawah dengan nama repo GitHub Pages kamu,
-// misal: base: '/congklak-game/'
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-});
